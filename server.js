2964d8f72e7459abf2afb6c74312136e69428f15
Caitlin Lindauer <clilos018@gmail.com>
2022-05-06 17:42:58 -0600

    added express code to server.js and created a utils folder

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const hndlb = require('express-handlebars');
+const Sequelize = require('connect-session-sequelize')(session.Store);
+
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const dates = require('./util/dates')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new Sequelize({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = hndlb.create({ dates });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes);
 
