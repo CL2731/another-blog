45a9bbb5bdc0f514f1deb9b900a304cbb4fa8757
Caitlin Lindauer <clilos018@gmail.com>
2022-06-12 22:24:18 -0600

    added routes to postRoutes

---
+++ routes/api/postRoutes.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const { Post } = require("../../models");
+const auth = require("../../utils/auth");
+
+router.post("/", auth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json({ post: newPost, message: "Post made" });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", auth, async (req, res) => {
+  try {
+    const update = await Post.update(
+      {
+        body: req.body.body,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    res.status(200).json({ post: update, message: "Updated" });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!post) {
+      res.status(400).json({ message: "Can not find post" });
+    }
+    res.status(200).json(post);
+  } catch (err) {
+    try {
+    } catch (error) {
+      res.status(500).json(err);
+    }
+  }
+});
+
+module.exports = router;
