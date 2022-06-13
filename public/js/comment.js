8849c8dc1cc7205433a020aac2a8ac39e0146d47
Caitlin Lindauer <clilos018@gmail.com>
2022-06-12 22:43:50 -0600

    added homepage.handlebars and some code

---
+++ public/js/comment.js
@@ -0,0 +1,19 @@
+const addComment = async (e) => {
+    e.preventDefault();
+    const body = document.querySelector("#commentInput").value.trim();
+    const post_id = window.location.href.split("/").pop();
+    if (body && post_id) {
+      const res = await fetch("/api/comments", {
+        method: "POST",
+        body: JSON.stringify({ body, post_id }),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (res.ok) {
+        document.location.replace(`/posts/${post_id}`);
+      } else {
+        res.json().then((data) => alert(data.message));
+      }
+    }
+  };
+  
+  $("#commentForm").on("submit", addComment);
