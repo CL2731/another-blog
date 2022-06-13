772bb665f3c791aff28ce3009746c3bc0394098f
Caitlin Lindauer <clilos018@gmail.com>
2022-06-12 22:02:33 -0600

    added some missing files

---
+++ routes/api/commentRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const auth = require("../../utils/auth");
+
+router.post("/", auth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res
+      .status(200)
+      .json([{ comment: newComment, message: "Made new comment" }]);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
